dd7b3d779cd8f3d165adf9ad772eee639a0b24a1
tote00 <2201100081be@cyber-u.ac.jp>
2024-12-05 02:40:35 +0900

    add yesno api

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/notes'); 
 var notesRouter = require('./routes/notes'); 
 var catRouter = require('./routes/cat');
+var yesnoRouter = require('./routes/yesno');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use('/users', usersRouter);
 app.use('/hello', helloRouter); 
 app.use('/notes', notesRouter);
 app.use('/cat', catRouter);
+app.use('/yesno', catRouter);
 
 
 // catch 404 and forward to error handler
--- routes/hello.js
+++ routes/hello.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET hello page. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, World!');
+  res.render('hello', { title: 'Hello Express 2' });
 });
 
 module.exports = router;
